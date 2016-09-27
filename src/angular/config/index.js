3812bbd02f5a6d6e1e4b25ac546b2a0295a7d399
feizheng <1290657123@qq.com>
2016-09-27 23:50:06 +0800

    initial

---
+++ src/angular/config/index.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+
+  angular.module('nx.widget').constant('pullToRefreshConfig', {
+    treshold: 60,
+    debounce: 400,
+    text: {
+      pull: 'pull to refresh',
+      release: 'release to refresh',
+      loading: 'refreshing...'
+    },
+    icon: {
+      pull: 'fa fa-arrow-down',
+      release: 'fa fa-arrow-up',
+      loading: 'fa fa-refresh fa-spin'
+    }
+  });
+
+})();
