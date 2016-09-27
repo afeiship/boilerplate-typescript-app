e9649481faa180f3b39352dc7b573ca59868b2bd
feizheng <1290657123@qq.com>
2016-09-27 23:55:19 +0800

    finish

---
+++ dist/js/angular-pull-to-refresh.js
@@ -0,0 +1,99 @@
+(function () {
+  'use strict';
+
+  angular.module('nx.widget', []);
+
+})();
+
+(function() {
+  'use strict';
+
+  angular.module('nx.widget').constant('pullToRefreshConfig', {
+    treshold: 60,
+    debounce: 400,
+    text: {
+      pull: 'pull to refresh',
+      release: 'release to refresh',
+      loading: 'refreshing...'
+    },
+    icon: {
+      pull: 'fa fa-arrow-down',
+      release: 'fa fa-arrow-up',
+      loading: 'fa fa-refresh fa-spin'
+    }
+  });
+
+})();
+
+(function() {
+  'use strict';
+
+  angular.module('nx.widget')
+    .directive('pullToRefresh', function($compile, $timeout, $q, pullToRefreshConfig) {
+      return {
+        scope: true,
+        restrict: 'A',
+        transclude: true,
+        templateUrl: '<div class="nx-widget-pull-to-refresh">' +
+          '<i ng-class="icon[status]"></i>&nbsp;' +
+          '<span ng-bind="text[status]"></span>' +
+          '</div>' +
+          '<div ng-transclude></div>',
+        compile: function compile(tElement, tAttrs, transclude) {
+
+          return function postLink(scope, iElement, iAttrs) {
+
+            var config = angular.extend({}, pullToRefreshConfig, iAttrs);
+            var scrollElement = iElement.parent();
+            var ptrElement = window.ptr = iElement.children()[0];
+
+            // Initialize isolated scope vars
+            scope.text = config.text;
+            scope.icon = config.icon;
+            scope.status = 'pull';
+
+            var setStatus = function(status) {
+              shouldReload = status === 'release';
+              scope.$apply(function() {
+                scope.status = status;
+              });
+            };
+
+            var shouldReload = false;
+            iElement.bind('touchmove', function(ev) {
+              var top = scrollElement[0].scrollTop;
+              if (top < -config.treshold && !shouldReload) {
+                setStatus('release');
+              } else if (top > -config.treshold && shouldReload) {
+                setStatus('pull');
+              }
+            });
+
+            iElement.bind('touchend', function(ev) {
+              if (!shouldReload) return;
+              ptrElement.style.webkitTransitionDuration = 0;
+              ptrElement.style.margin = '0 auto';
+              setStatus('loading');
+
+              var start = +new Date();
+              $q.when(scope.$eval(iAttrs.pullToRefresh))
+                .then(function() {
+                  var elapsed = +new Date() - start;
+                  $timeout(function() {
+                    ptrElement.style.margin = '';
+                    ptrElement.style.webkitTransitionDuration = '';
+                    scope.status = 'pull';
+                  }, elapsed < config.debounce ? config.debounce - elapsed : 0);
+                });
+            });
+
+            scope.$on('$destroy', function() {
+              iElement.unbind('touchmove');
+              iElement.unbind('touchend');
+            });
+
+          };
+        }
+      };
+    });
+})();
