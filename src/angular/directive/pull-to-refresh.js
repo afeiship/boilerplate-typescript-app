8c9f7f5684f550a64f0c7d20ac03029697270a63
feizheng <1290657123@qq.com>
2016-10-03 15:05:55 +0800

    update code

--- src/angular/directive/pull-to-refresh.js
+++ src/angular/directive/pull-to-refresh.js
@@ -1,6 +1,8 @@
 (function() {
   'use strict';
 
+
+
   angular.module('nx.widget')
     .directive('nxPullToRefresh', function($compile, $timeout, $q, nxPullToRefreshConfig) {
       return {
@@ -15,10 +17,13 @@
         compile: function compile(tElement, tAttrs, transclude) {
 
           return function postLink(scope, iElement, iAttrs) {
-
+            var lock, draging;
+            var start, end;
             var config = angular.extend({}, nxPullToRefreshConfig, iAttrs);
             var scrollElement = iElement.parent();
             var ptrElement = window.ptr = iElement.children()[0];
+            var offset = config.treshold;
+            var callback = scope.$eval(iAttrs.pullToRefresh);
 
             // Initialize isolated scope vars
             scope.text = config.text;
@@ -33,37 +38,67 @@
             };
 
             var shouldReload = false;
-            iElement.bind('touchmove', function(ev) {
-              var top = scrollElement[0].scrollTop;
-              if (top < -config.treshold && !shouldReload) {
-                setStatus('release');
-              } else if (top > -config.treshold && shouldReload) {
-                setStatus('pull');
+
+            iElement.bind('touchstart', function(inEvent) {
+              lock = true;
+              draging = true;
+              start = inEvent.touches[0].pageY;
+              setTranslition(0);
+            });
+
+
+            iElement.bind('touchmove', function(inEvent) {
+              if (scrollElement[0].scrollTop <= 0 && draging) {
+                end = inEvent.touches[0].pageY;
+                if (start < end) {
+                  inEvent.preventDefault();
+                  setTranslition(0);
+                  translate(end - start - offset);
+                }
               }
             });
 
-            iElement.bind('touchend', function(ev) {
-              if (!shouldReload) return;
-              ptrElement.style.webkitTransitionDuration = 0;
-              ptrElement.style.margin = '0 auto';
-              setStatus('loading');
-
-              var start = +new Date();
-              $q.when(scope.$eval(iAttrs.nxPullToRefresh))
-                .then(function() {
-                  var elapsed = +new Date() - start;
-                  $timeout(function() {
-                    ptrElement.style.margin = '';
-                    ptrElement.style.webkitTransitionDuration = '';
-                    scope.status = 'pull';
-                  }, elapsed < config.debounce ? config.debounce - elapsed : 0);
-                });
+            iElement.bind('touchend', function(inEvent) {
+              if (draging) {
+                draging = false;
+                if (end - start >= offset) {
+                  setTranslition(1);
+                  translate(0);
+                  if (typeof callback == "function") {
+                    callback();
+                  }
+                } else {
+                  console.log('reset!!!');
+                  reset();
+                }
+              }
             });
 
             scope.$on('$destroy', function() {
+              iElement.unbind('touchstart');
               iElement.unbind('touchmove');
               iElement.unbind('touchend');
             });
+
+            function setTranslition(time) {
+              iElement.css({
+                "-webkit-transition": "all " + time + "s",
+                "transition": "all " + time + "s"
+              });
+            }
+
+            function reset() {
+              translate(0 - offset);
+              lock = false;
+            }
+
+            function translate(inDiff) {
+              iElement.css({
+                "-webkit-transform": "translate(0," + inDiff + "px)",
+                "transform": "translate(0," + inDiff + "px)"
+              });
+            }
+
 
           };
         }
