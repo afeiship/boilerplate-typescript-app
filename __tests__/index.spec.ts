9378d65236b17f91b31e82205208626b63470057
afeiship <1290657123@qq.com>
2023-02-09 13:35:17 +0800

    refactor(test): rename fn to capwords

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,4 +1,4 @@
-import fn from '../src';
+import capwords from '../src';
 /*
 freeCodeCamp is an awesome resource
 freeCodeCamp.is.an.awesome.resource
@@ -8,22 +8,30 @@ freeCodeCamp-is-an-awesome-resource
 
 describe('api.basic', () => {
   test('should return a string', () => {
-    expect(typeof fn('freeCodeCamp')).toBe('string');
+    expect(typeof capwords('freeCodeCamp')).toBe('string');
   });
 
   test('freeCodeCamp is an awesome resource', () => {
-    expect(fn('freeCodeCamp is an awesome resource')).toBe('FreeCodeCamp Is An Awesome Resource');
+    expect(capwords('freeCodeCamp is an awesome resource')).toBe(
+      'FreeCodeCamp Is An Awesome Resource'
+    );
   });
 
   test('freeCodeCamp.is.an.awesome.resource', () => {
-    expect(fn('freeCodeCamp.is.an.awesome.resource')).toBe('FreeCodeCamp Is An Awesome Resource');
+    expect(capwords('freeCodeCamp.is.an.awesome.resource')).toBe(
+      'FreeCodeCamp Is An Awesome Resource'
+    );
   });
 
   test('freeCodeCamp_is_an_awesome_resource', () => {
-    expect(fn('freeCodeCamp_is_an_awesome_resource')).toBe('FreeCodeCamp Is An Awesome Resource');
+    expect(capwords('freeCodeCamp_is_an_awesome_resource')).toBe(
+      'FreeCodeCamp Is An Awesome Resource'
+    );
   });
 
   test('freeCodeCamp-is-an-awesome-resource', () => {
-    expect(fn('freeCodeCamp-is-an-awesome-resource')).toBe('FreeCodeCamp Is An Awesome Resource');
+    expect(capwords('freeCodeCamp-is-an-awesome-resource')).toBe(
+      'FreeCodeCamp Is An Awesome Resource'
+    );
   });
 });
